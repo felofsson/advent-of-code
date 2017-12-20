09a701bd6920557bc2e38efb52f24600ce5aeea8
felofsson <elofsson.fredrik@gmail.com>
2017-12-20 17:25:53 +0100

    day07

---
+++ Day07/main_day07.py
@@ -0,0 +1,29 @@
+
+
+if __name__ == "__main__":
+
+    f = open('data.txt', 'r')
+    instructions = f.read().split(chr(10))
+    f.close()
+
+    grand_parent = None
+
+    # Parse the list of instructions twice
+    instructions.extend(instructions)
+
+    for line in instructions:
+
+        parent_and_child = line.split("->")
+        parent = parent_and_child[0].split(" ")[0]
+
+        if grand_parent is None:
+            grand_parent = parent
+
+        if len(parent_and_child) > 1:
+            children = list(map(lambda x: x.lstrip(), parent_and_child[1].split(",")))
+
+            if grand_parent in children:
+                grand_parent = parent
+
+    print("grand_parent=", grand_parent)
+    
